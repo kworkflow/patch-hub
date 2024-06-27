45fda2734b5b8ca35446b30072d5a27c41587f41
David Tadokoro <davidbtadokoro@usp.br>
2024-06-27 19:17:46 -0300

    src: main: Implement brute force pagination to Lore request for patch feed
    Because Lore limits the response to a request for patch feed to 200
    patches, in other words, it paginates the *response* for a given
    request, we need to implement a mechanism that can (potentially) fetch
    and process all the pages of a response.
    
    To accomplish this, implement a brute force logic inside the crate root
    for the main binary crate to iterate through the pages and process
    patches until it processes, at least, the number of representative
    patches required by the user.
    
    The implementation manipulates the paramater `o` of the Lore API, which,
    for patch feed requests, indicates the minimum exclusive index of the
    first patch from the response. More specifically, it repeats the process
    of fetching a page, processing the patches from the page, updating the
    representative patches, and adding 200 to the value of `o`.
    
    Signed-off-by: David Tadokoro <davidbtadokoro@usp.br>

--- src/main.rs
+++ src/main.rs
@@ -2,29 +2,41 @@ use lore_peek::lore_session::LoreSession;
 use lore_peek::lore_api_client::{FailedFeedResquest, LoreAPIClient};
 use lore_peek::patch::PatchFeed;
 use serde_xml_rs::from_str;
-use std::{env, u32};
+use std::env;
+
+const LORE_PAGE_SIZE: u32 = 200;
 
 fn main() {
     let args: Vec<_> = env::args().collect();
     let mut lore_session: LoreSession = LoreSession::new();
-    let patch_feed: PatchFeed;
-    let processed_patches_ids: Vec<String>;
+    let mut patch_feed: PatchFeed;
+    let mut processed_patches_ids: Vec<String>;
+    let target_list: String;
+    let n_patchsets: u32;
+    let mut min_index: u32 = 0;
 
     if args.len() != 3 {
-        panic!("Error: Wrong number\nUsage: cargo run <target_list> <min_index>");
+        panic!("Error: Wrong number\nUsage: cargo run <target_list> <n_patchsets>");
     }
 
-    match LoreAPIClient::request_patch_feed(&String::from(&args[1]), args[2].parse::<u32>().unwrap()) {
-        Ok(feed_response_body) => patch_feed = from_str(&feed_response_body).unwrap(),
-        Err(failed_feed_request) => match failed_feed_request {
-            FailedFeedResquest::UnknowError(error) => panic!("{error:#?}"),
-            FailedFeedResquest::StatusNotOk(status_code) => panic!("Lore request returned status code {status_code}"),
-            FailedFeedResquest::EndOfFeed => panic!("End of feed"),
-        },
-    }
+    target_list = String::from(&args[1]);
+    n_patchsets = args[2].parse::<u32>().unwrap();
 
-    processed_patches_ids = lore_session.process_patches(patch_feed);
-    lore_session.update_representative_patches(processed_patches_ids);
+    while lore_session.get_representative_patches_ids().len() < usize::try_from(n_patchsets).unwrap() {
+        match LoreAPIClient::request_patch_feed(&target_list, min_index) {
+            Ok(feed_response_body) => patch_feed = from_str(&feed_response_body).unwrap(),
+            Err(failed_feed_request) => match failed_feed_request {
+                FailedFeedResquest::UnknowError(error) => panic!("{error:#?}"),
+                FailedFeedResquest::StatusNotOk(status_code) => panic!("Lore request returned status code {status_code}"),
+                FailedFeedResquest::EndOfFeed => break,
+            },
+        }
+
+        processed_patches_ids = lore_session.process_patches(patch_feed);
+        lore_session.update_representative_patches(processed_patches_ids);
+
+        min_index = min_index + LORE_PAGE_SIZE;
+    }
 
     let representative_patches_ids: &Vec<String> = lore_session.get_representative_patches_ids();
     println!("Number of representative patches processed: {}", representative_patches_ids.len());
