608eb832f36baf1e8629f4c60271429b3070d524
David Tadokoro <davidbtadokoro@usp.br>
2024-06-27 00:56:59 -0300

    src: main: Implement dynamic single request to Lore with brute force
    Currently, `lore_peek` uses a pre-fetched Lore response as the input for
    the processing of representative patches.
    
    In this context, implement a dynamic single HTTP request to Lore API to
    fetch an Atom feed containing the last patches sent to a target list
    that is passed as argument. The request is made using `reqwest`, and the
    logic handles the three necessary cases:
    
      1. 200 OK returning a non-empty Atom feed (success case);
      2. 200 OK returning an empty Atom feed (normal fail case);
      3. Not 200 OK (abnormal fail case).
    
    This is implemented completely in the crate root of the main binary
    crate, which points to the brute force used. In the future we should
    extract this logic to a dedicated type, and maybe a dedicated module.
    
    Signed-off-by: David Tadokoro <davidbtadokoro@usp.br>

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,43 @@
 use lore_peek::lore_session::LoreSession;
 use lore_peek::patch::PatchFeed;
+use reqwest::blocking::Response;
 use serde_xml_rs::from_str;
-use std::{env, fs};
+use std::env;
+
+const LORE_DOMAIN: &str = r"https://lore.kernel.org/";
 
 fn main() {
     let args: Vec<_> = env::args().collect();
     let mut lore_session: LoreSession = LoreSession::new();
     let patch_feed: PatchFeed;
     let processed_patches_ids: Vec<String>;
+    let mut lore_api_request: String = String::from(LORE_DOMAIN);
+    let lore_api_response: Response;
+    let lore_api_response_body: String;
 
     if args.len() != 2 {
         panic!("Need 1 arg");
     }
 
-    patch_feed = from_str(&fs::read_to_string(&String::from(&args[1])).unwrap()).unwrap();
+    lore_api_request.push_str(&String::from(&args[1]));
+    lore_api_request.push_str(r"/?x=A&q=((s:patch+OR+s:rfc)+AND+NOT+s:re:)&o=0");
+
+    lore_api_response = match reqwest::blocking::get(lore_api_request) {
+        Ok(response) => response,
+        Err(_) => panic!("Something weird happened!"),
+    };
+
+    match lore_api_response.status().as_u16() {
+        200 => (),
+        _ => panic!("HTTP request didn't return 200 OK"),
+    };
+
+    lore_api_response_body = lore_api_response.text().unwrap();
+    if lore_api_response_body.eq(r"</feed>") {
+        panic!("No more patches")
+    };
+
+    patch_feed = from_str(&lore_api_response_body).unwrap();
     processed_patches_ids = lore_session.process_patches(patch_feed);
     lore_session.update_representative_patches(processed_patches_ids);
 
