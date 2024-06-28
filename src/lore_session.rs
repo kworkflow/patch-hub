dc95f101e7210cbb9a1f2522e0f973ccc2631fb3
David Tadokoro <davidbtadokoro@usp.br>
2024-06-28 19:22:24 -0300

    src: lore_session: Extract pagination Lore request for patch feed into `LoreSession` method
    Extract all the logic pertaining to process the paginated responses of
    a Lore request for the patch feed of a target list to the method
    `process_n_representative_patches` of the `LoreSession` type.
    
    This extraction made possible the encapsulation of practically all
    implementation details and dependencies from the crate root of the main
    binary crate. More specifically, the only type needed for a caller that
    wishes to fetch a number of representative patches is the `LoreSession`,
    and the `serde_xml_rs` dependency (responsible for deserializing the
    patch feed) is encapsulated into the `lore_session` module. Also, the
    public methods `process_patches` and `update_representative_patches` are
    turned into private, as they are now only called by the extracted
    method.
    
    The only downside is that now we need to incorporate the values of `o`,
    i.e., the `min_index`, and of the target list into the `LoreSession`
    type.
    
    Signed-off-by: David Tadokoro <davidbtadokoro@usp.br>

--- src/lore_session.rs
+++ src/lore_session.rs
@@ -1,19 +1,26 @@
 use crate::patch::{Patch, PatchFeed, PatchRegex};
+use crate::lore_api_client::{FailedFeedResquest, LoreAPIClient};
 use std::collections::HashMap;
+use serde_xml_rs::from_str;
 
+const LORE_PAGE_SIZE: u32 = 200;
 
 pub struct LoreSession {
     representative_patches_ids: Vec<String>,
     processed_patches_map: HashMap<String, Patch>,
     patch_regex: PatchRegex,
+    target_list: String,
+    min_index: u32,
 }
 
 impl LoreSession {
-    pub fn new() -> LoreSession {
+    pub fn new(target_list: String) -> LoreSession {
         LoreSession {
+            target_list: target_list,
             representative_patches_ids: Vec::new(),
             processed_patches_map: HashMap::new(),
             patch_regex: PatchRegex::new(),
+            min_index: 0,
         }
     }
 
@@ -25,7 +32,28 @@ impl LoreSession {
         self.processed_patches_map.get(message_id)
     }
 
-    pub fn process_patches(self: &mut Self, patch_feed: PatchFeed) -> Vec<String> {
+    pub fn process_n_representative_patches(self: &mut Self,n: u32) {
+        let mut patch_feed: PatchFeed;
+        let mut processed_patches_ids: Vec<String>;
+
+        while self.representative_patches_ids.len() < usize::try_from(n).unwrap() {
+            match LoreAPIClient::request_patch_feed(&self.target_list, self.min_index) {
+                Ok(feed_response_body) => patch_feed = from_str(&feed_response_body).unwrap(),
+                Err(failed_feed_request) => match failed_feed_request {
+                    FailedFeedResquest::UnknowError(error) => panic!("{error:#?}"),
+                    FailedFeedResquest::StatusNotOk(status_code) => panic!("Lore request returned status code {status_code}"),
+                    FailedFeedResquest::EndOfFeed => break,
+                },
+            }
+
+            processed_patches_ids = self.process_patches(patch_feed);
+            self.update_representative_patches(processed_patches_ids);
+
+            self.min_index = self.min_index + LORE_PAGE_SIZE;
+        }
+    }
+
+    fn process_patches(self: &mut Self, patch_feed: PatchFeed) -> Vec<String> {
         let mut processed_patches_ids: Vec<String> = Vec::new();
 
         for mut patch in patch_feed.get_patches() {
@@ -40,7 +68,7 @@ impl LoreSession {
         processed_patches_ids
     }
 
-    pub fn update_representative_patches(self: &mut Self, processed_patches_ids: Vec<String>) {
+    fn update_representative_patches(self: &mut Self, processed_patches_ids: Vec<String>) {
         let mut patch: &Patch;
         let mut patch_number_in_series: u32;
 
--- src/main.rs
+++ src/main.rs
@@ -1,42 +1,21 @@
 use lore_peek::lore_session::LoreSession;
-use lore_peek::lore_api_client::{FailedFeedResquest, LoreAPIClient};
-use lore_peek::patch::PatchFeed;
-use serde_xml_rs::from_str;
 use std::env;
-
-const LORE_PAGE_SIZE: u32 = 200;
 
 fn main() {
     let args: Vec<_> = env::args().collect();
-    let mut lore_session: LoreSession = LoreSession::new();
-    let mut patch_feed: PatchFeed;
-    let mut processed_patches_ids: Vec<String>;
     let target_list: String;
-    let n_patchsets: u32;
-    let mut min_index: u32 = 0;
+    let n: u32;
+    let mut lore_session: LoreSession;
 
     if args.len() != 3 {
-        panic!("Error: Wrong number\nUsage: cargo run <target_list> <n_patchsets>");
+        panic!("Error: Wrong number\nUsage: cargo run <target_list> <n>");
     }
 
     target_list = String::from(&args[1]);
-    n_patchsets = args[2].parse::<u32>().unwrap();
+    n = args[2].parse::<u32>().unwrap();
 
-    while lore_session.get_representative_patches_ids().len() < usize::try_from(n_patchsets).unwrap() {
-        match LoreAPIClient::request_patch_feed(&target_list, min_index) {
-            Ok(feed_response_body) => patch_feed = from_str(&feed_response_body).unwrap(),
-            Err(failed_feed_request) => match failed_feed_request {
-                FailedFeedResquest::UnknowError(error) => panic!("{error:#?}"),
-                FailedFeedResquest::StatusNotOk(status_code) => panic!("Lore request returned status code {status_code}"),
-                FailedFeedResquest::EndOfFeed => break,
-            },
-        }
-
-        processed_patches_ids = lore_session.process_patches(patch_feed);
-        lore_session.update_representative_patches(processed_patches_ids);
-
-        min_index = min_index + LORE_PAGE_SIZE;
-    }
+    lore_session = LoreSession::new(target_list);
+    lore_session.process_n_representative_patches(n);
 
     let representative_patches_ids: &Vec<String> = lore_session.get_representative_patches_ids();
     println!("Number of representative patches processed: {}", representative_patches_ids.len());
