21a9b40f3395bb1254cba532755b3ff7a385bbd8
David Tadokoro <davidbtadokoro@usp.br>
2024-06-29 01:48:55 -0300

    src: main: Introduce proof of concept of front-end client
    Just using the model laid out by the previous commits, introduce
    a simple terminal front-end (view) client that uses the model
    represented by the `LoreSession` type to display the feed of
    representative patches in pages. This effectively turns the main binary
    crate into the first draft of the `lore_peek` app.
    
    In this starting draft of an app, the user can consult the sequence of
    representative patches of the patch feed from a target mailing list.
    This sequence is partitioned in same sized pages, which the user can
    navigate to older (forward) and to newest (backwards) pages at runtime,
    and gracefully quit the application. At last, the user defines the
    target list and page size by passing CLI arguments. The processing of
    older pages happens on demand, and pages already processed don't get
    processed again.
    
    Note that we aim to use a real TUI framework in the future. Right now,
    I have in mind the `ratatui.rs` crate, as it has a great look and feel,
    and seems to be versatile and powerful enough for our needs, while still
    being lightweight.
    
    Signed-off-by: David Tadokoro <davidbtadokoro@usp.br>

--- src/main.rs
+++ src/main.rs
@@ -2,48 +2,94 @@ use lore_peek::lore_session::LoreSession;
 use lore_peek::patch::Patch;
 use lore_peek::lore_api_client::FailedFeedRequest;
 use std::env;
+use std::io::{self, Write};
+use std::process::exit;
 
 fn main() {
+    let target_list: String;
+    let page_size: u32;
+
+    (target_list, page_size) = parse_args();
+    main_loop(target_list, page_size);
+}
+
+fn parse_args() -> (String, u32) {
     let args: Vec<_> = env::args().collect();
     let target_list: String;
     let page_size: u32;
-    let page_number: u32;
-    let mut lore_session: LoreSession;
-    let patch_feed_page: Vec<&Patch>;
-    let mut index: u32;
 
-    if args.len() != 4 {
-        panic!("[errno::EINVAL]\n*\tWrong number of arguments\n*\tUsage: cargo run <target_list> <page_size> <page_number>");
+    if args.len() != 3 {
+        panic!("[errno::EINVAL]\n*\tWrong number of arguments\n*\tUsage: cargo run <target_list> <page_size>");
     }
 
     target_list = String::from(&args[1]);
     page_size = args[2].parse::<u32>().unwrap();
-    page_number = args[3].parse::<u32>().unwrap();
 
-    if (page_size == 0) || (page_number == 0) {
-        panic!("[errno::EINVAL]\n*\tpage_size and page_number should be non-zero positives")
+    if page_size == 0 {
+        panic!("[errno::EINVAL]\n*\tpage_size should be non-zero positives")
     }
 
-    lore_session = LoreSession::new(target_list);
-    if let Err(failed_feed_request) = lore_session.process_n_representative_patches(page_size * page_number) {
-        match failed_feed_request {
-            FailedFeedRequest::UnknownError(error) => panic!("[FailedFeedRequest::UnknownError]\n*\tFailed to request feed\n*\t{error:#?}"),
-            FailedFeedRequest::StatusNotOk(feed_response) => panic!("[FailedFeedRequest::StatusNotOk]\n*\tRequest returned with non-OK status\n*\t{feed_response:#?}"),
-            FailedFeedRequest::EndOfFeed => panic!("[FailedFeedRequest::EndOfFeed]\n*\tReached end of feed"),
+    (target_list, page_size)
+}
+
+fn main_loop(target_list: String, page_size: u32) {
+    let mut lore_session: LoreSession;
+    let mut page_number: u32 = 1;
+    let mut patch_feed_page: Vec<&Patch>;
+    let mut index: u32;
+    let mut command_code: char;
+    let mut input: String = String::new();
+
+    lore_session = LoreSession::new(target_list.clone());
+
+    loop {
+        if let Err(failed_feed_request) = lore_session.process_n_representative_patches(page_size * page_number) {
+            match failed_feed_request {
+                FailedFeedRequest::UnknownError(error) => panic!("[FailedFeedRequest::UnknownError]\n*\tFailed to request feed\n*\t{error:#?}"),
+                FailedFeedRequest::StatusNotOk(feed_response) => panic!("[FailedFeedRequest::StatusNotOk]\n*\tRequest returned with non-OK status\n*\t{feed_response:#?}"),
+                FailedFeedRequest::EndOfFeed => panic!("[FailedFeedRequest::EndOfFeed]\n*\tReached end of feed"),
+            }
+        };
+
+        println!("======================= {target_list} pg. {page_number} =======================");
+        patch_feed_page = lore_session.get_patch_feed_page(page_size, page_number);
+        index = page_size * (page_number - 1);
+        for patch in patch_feed_page {
+            println!(
+                "{:03}. V{} | #{:02} | {} | {}",
+                index, patch.get_version(), patch.get_total_in_series(), patch.get_title(), patch.get_author().name
+            );
+
+            index += 1;
         }
-    };
+        println!("======================= {target_list} pg. {page_number} =======================\n");
 
-    println!("Number of representative patches processed: {}", lore_session.get_representative_patches_ids().len());
+        loop {
+            input.clear();
 
-    patch_feed_page = lore_session.get_patch_feed_page(page_size, page_number);
-    index = page_size * (page_number - 1);
-    for patch in patch_feed_page {
+            print!("Enter a command [ (n)ext | (p)revious | (q)uit]: ");
+            io::stdout().flush().unwrap();
+            io::stdin().read_line(&mut input).unwrap();
+            if input.len() == 2 {
+                if let Some(char) = input.trim().chars().next() {
+                    match char {
+                        'N' | 'n' | 'P' | 'p' | 'Q' | 'q' => {
+                            command_code = char;
+                            break;
+                        },
+                        _ => (),
+                    }
+                }
+            };
 
-        println!(
-            "{:03}. V{} | #{:02} | {} | {}",
-            index, patch.get_version(), patch.get_total_in_series(), patch.get_title(), patch.get_author().name
-        );
+            println!("Invalid input!");
+        }
 
-        index += 1;
+        match command_code {
+            'N' | 'n' => page_number += 1,
+            'P' | 'p' => if page_number != 1 { page_number -= 1 },
+            'Q' | 'q' => exit(0),
+            _ => panic!("[errno::EINVAL]\n*\tInvalid command. It shouldn't get to here...")
+        }
     }
 }
