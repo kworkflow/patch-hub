6fdf82bf6203cb9c0cc41f64f4edab61e57d448b
David Tadokoro <davidbtadokoro@usp.br>
2024-08-13 13:00:40 -0300

    feat: add persistent configurations through file
    Currently, `patch-hub` configurations have default values that can be
    overriden by environment variables.
    
    Add persistent configurations through a JSON file. The location of this
    file is set by the `PATCH_HUB_CONFIG_PATH` environment variable and
    defaults to `${HOME}/.local/share/patch_hub/config.json`.
    
    This commit also enforces the precedence of configurations, which is
    (from lower to higher):
    
    1. Default values - function `Config::default()`
    2. Persistent file - function `Config::detect_patch_hub_config_file()`
    3. Environment variables - function `Config::override_with_env_vars()`
    
    Closes: #23
    
    Signed-off-by: David Tadokoro <davidbtadokoro@usp.br>

--- src/app/config.rs
+++ src/app/config.rs
@@ -1,8 +1,15 @@
-use std::env;
+use serde::{Deserialize, Serialize};
+use std::{
+    env,
+    fs::{self, File},
+    io,
+    path::Path,
+};
 
 #[cfg(test)]
 mod tests;
 
+#[derive(Serialize, Deserialize)]
 pub struct Config {
     page_size: usize,
     patchsets_cache_dir: String,
@@ -14,37 +21,71 @@ pub struct Config {
 }
 
 impl Config {
-    pub fn build() -> Self {
-        let page_size: usize = match env::var("PATCH_HUB_PAGE_SIZE") {
-            Ok(value) => value.parse().unwrap(),
-            Err(_) => 30,
-        };
-
-        let cache_dir = match env::var("PATCH_HUB_CACHE_DIR") {
-            Ok(value) => value,
-            Err(_) => format!("{}/.cache/patch_hub", env::var("HOME").unwrap()),
-        };
-
-        let patchsets_cache_dir = format!("{cache_dir}/patchsets");
-
-        let data_dir = match env::var("PATCH_HUB_DATA_DIR") {
-            Ok(value) => value,
-            Err(_) => format!("{}/.local/share/patch_hub", env::var("HOME").unwrap()),
-        };
-
-        let bookmarked_patchsets_path = format!("{data_dir}/bookmarked_patchsets.json");
-        let mailing_lists_path = format!("{data_dir}/mailing_lists.json");
-        let reviewed_patchsets_path = format!("{data_dir}/reviewed_patchsets.json");
-        let logs_path = format!("{data_dir}/logs");
+    fn default() -> Self {
+        let cache_dir = format!("{}/.cache/patch_hub", env::var("HOME").unwrap());
+        let data_dir = format!("{}/.local/share/patch_hub", env::var("HOME").unwrap());
 
         Config {
-            page_size,
-            patchsets_cache_dir,
-            bookmarked_patchsets_path,
-            mailing_lists_path,
-            reviewed_patchsets_path,
-            logs_path,
+            page_size: 30,
+            patchsets_cache_dir: format!("{cache_dir}/patchsets"),
+            bookmarked_patchsets_path: format!("{data_dir}/bookmarked_patchsets.json"),
+            mailing_lists_path: format!("{data_dir}/mailing_lists.json"),
+            reviewed_patchsets_path: format!("{data_dir}/reviewed_patchsets.json"),
+            logs_path: format!("{data_dir}/logs"),
         }
+    }
+
+    fn detect_patch_hub_config_file() -> Option<Config> {
+        if let Ok(config_path) = env::var("PATCH_HUB_CONFIG_PATH") {
+            if Path::new(&config_path).is_file() {
+                let file_contents = fs::read_to_string(&config_path).unwrap_or(String::new());
+                if let Ok(config) = serde_json::from_str(&file_contents) {
+                    return Some(config);
+                }
+            }
+        }
+
+        let config_path = format!(
+            "{}/.local/share/patch_hub/config.json",
+            env::var("HOME").unwrap()
+        );
+        if Path::new(&config_path).is_file() {
+            let file_contents = fs::read_to_string(&config_path).unwrap_or(String::new());
+            if let Ok(config) = serde_json::from_str(&file_contents) {
+                return Some(config);
+            }
+        }
+
+        None
+    }
+
+    fn override_with_env_vars(&mut self) {
+        if let Ok(page_size) = env::var("PATCH_HUB_PAGE_SIZE") {
+            self.page_size = page_size.parse().unwrap();
+        };
+
+        if let Ok(cache_dir) = env::var("PATCH_HUB_CACHE_DIR") {
+            self.patchsets_cache_dir = format!("{cache_dir}/patchsets");
+        };
+
+        if let Ok(data_dir) = env::var("PATCH_HUB_DATA_DIR") {
+            self.bookmarked_patchsets_path = format!("{data_dir}/bookmarked_patchsets.json");
+            self.mailing_lists_path = format!("{data_dir}/mailing_lists.json");
+            self.reviewed_patchsets_path = format!("{data_dir}/reviewed_patchsets.json");
+            self.logs_path = format!("{data_dir}/logs");
+        };
+    }
+
+    pub fn build() -> Self {
+        let mut config = Self::default();
+
+        if let Some(config_from_file) = Self::detect_patch_hub_config_file() {
+            config = config_from_file;
+        }
+
+        config.override_with_env_vars();
+
+        config
     }
 
     pub fn get_page_size(&self) -> usize {
@@ -69,5 +110,25 @@ impl Config {
 
     pub fn get_logs_path(&self) -> &str {
         &self.logs_path
+    }
+
+    #[allow(dead_code)]
+    pub fn save_patch_hub_config(&self) -> io::Result<()> {
+        let config_path = if let Ok(path) = env::var("PATCH_HUB_CONFIG_PATH") {
+            path
+        } else {
+            format!(
+                "{}/.local/share/patch_hub/config.json",
+                env::var("HOME").unwrap()
+            )
+        };
+
+        let tmp_filename = format!("{}.tmp", config_path);
+        {
+            let tmp_file = File::create(&tmp_filename)?;
+            serde_json::to_writer_pretty(tmp_file, self)?;
+        }
+        fs::rename(tmp_filename, config_path)?;
+        Ok(())
     }
 }
--- src/app/config/tests.rs
+++ src/app/config/tests.rs
@@ -5,7 +5,7 @@ use std::sync::Mutex;
 static TEST_LOCK: Mutex<()> = Mutex::new(());
 
 #[test]
-fn can_build() {
+fn can_build_with_default_values() {
     let _lock = TEST_LOCK.lock().unwrap();
 
     env::set_var("HOME", "/fake/home/path");
@@ -35,14 +35,43 @@ fn can_build() {
 }
 
 #[test]
-fn can_build_with_custom_values() {
+fn can_build_with_config_file() {
     let _lock = TEST_LOCK.lock().unwrap();
 
+    env::set_var(
+        "PATCH_HUB_CONFIG_PATH",
+        "src/test_samples/app/config/config.json",
+    );
+    let config = Config::build();
+    env::remove_var("PATCH_HUB_CONFIG_PATH");
+
+    assert_eq!(1234, config.get_page_size());
+    assert_eq!("/cachedir/path", config.get_patchsets_cache_dir());
+    assert_eq!(
+        "/bookmarked/patchsets/path",
+        config.get_bookmarked_patchsets_path()
+    );
+    assert_eq!("/mailing/lists/path", config.get_mailing_lists_path());
+    assert_eq!(
+        "/reviewed/patchsets/path",
+        config.get_reviewed_patchsets_path()
+    );
+    assert_eq!("/logs/path", config.get_logs_path());
+}
+
+#[test]
+fn can_build_with_env_vars() {
+    let _lock = TEST_LOCK.lock().unwrap();
+
+    env::set_var("PATCH_HUB_PAGE_SIZE", "42");
     env::set_var("PATCH_HUB_CACHE_DIR", "/fake/cache/path");
     env::set_var("PATCH_HUB_DATA_DIR", "/fake/data/path");
     let config = Config::build();
+    env::remove_var("PATCH_HUB_PAGE_SIZE");
+    env::remove_var("PATCH_HUB_CACHE_DIR");
+    env::remove_var("PATCH_HUB_DATA_DIR");
 
-    assert_eq!(30, config.get_page_size());
+    assert_eq!(42, config.get_page_size());
     assert_eq!(
         "/fake/cache/path/patchsets",
         config.get_patchsets_cache_dir()
@@ -63,4 +92,30 @@ fn can_build_with_custom_values() {
 
     env::remove_var("PATCH_HUB_CACHE_DIR");
     env::remove_var("PATCH_HUB_DATA_DIR");
+}
+
+#[test]
+fn test_config_precedence() {
+    let _lock = TEST_LOCK.lock().unwrap();
+
+    // Default values
+    env::set_var("HOME", "/fake/home/path");
+    let config = Config::build();
+    assert_eq!(30, config.get_page_size());
+
+    // Config file should have precedence over default values
+    env::set_var(
+        "PATCH_HUB_CONFIG_PATH",
+        "src/test_samples/app/config/config.json",
+    );
+    let config = Config::build();
+    assert_eq!(1234, config.get_page_size());
+
+    // Env vars should have precedence over default values
+    env::set_var("PATCH_HUB_PAGE_SIZE", "42");
+    let config = Config::build();
+    assert_eq!(42, config.get_page_size());
+
+    env::remove_var("PATCH_HUB_CONFIG_PATH");
+    env::remove_var("PATCH_HUB_PAGE_SIZE");
 }
