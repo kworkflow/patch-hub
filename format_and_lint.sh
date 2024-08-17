2fa99c94c5f52b0ab4cd3ac46b43a4f92eee0976
Lorenzo Bertin Salvador <lorenzobs@usp.br>
2024-08-17 11:08:20 -0300

    feat: run rust-clippy in format_and_lint.sh
    rust-clippy is a rust linter, so it aims to improve
    general code quality.
    
    Signed-off-by: Lorenzo Bertin Salvador <lorenzobs@usp.br>

--- format_and_lint.sh
+++ format_and_lint.sh
@@ -3,11 +3,23 @@
 # making bash exit if anything fails
 set -e
 
+CLIPPY_CONFIGS="
+--allow=clippy::too-many-arguments \
+--deny=warnings \
+--deny=clippy::map_unwrap_or \
+--deny=unconditional_recursion
+"
+
 function format () {
 	echo "Formatting..."
 	cargo fmt --all
 }
 
-# TODO: run rust-clippy
+function lint () {
+	echo "Running clippy..."
+	cargo clippy --all-features --all-targets --tests -- $CLIPPY_CONFIGS
+}
 
-format
+format && 
+lint && 
+echo "format_and_lint.sh finished successfully"
