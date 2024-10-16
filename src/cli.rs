25cd55bb236bc4ddbf3cb697165a2cc4ff8d581d
OJarrisonn <j.h.m.t.v.10@gmail.com>
2024-10-16 17:06:10 -0300

    feat: add `-c | --show-configs` option
    Introduce a new `-c | --show-configs` option to patch-hub.
    
    This flag will simply print the config options patch-hub would use to
    the terminal as a json string and exit the application.
    
    Closes: #60
    
    [Maintainer edits]
    - Add short option `-c`
    - Add preamble "patch-hub configurations:" in printing
    - Add closes tag
    
    Signed-off-by: OJarrisonn <j.h.m.t.v.10@gmail.com>
    Reviewed-by: David Tadokoro <davidbtadokoro@usp.br>
    Signed-off-by: David Tadokoro <davidbtadokoro@usp.br>

--- src/cli.rs
+++ src/cli.rs
@@ -1,5 +1,45 @@
+use std::ops::ControlFlow;
+
 use clap::Parser;
+use color_eyre::eyre::eyre;
+use ratatui::{prelude::Backend, Terminal};
+
+use crate::{
+    app::{logging::Logger, App},
+    utils,
+};
 
 #[derive(Debug, Parser)]
 #[command(version, about)]
-pub struct Cli;
+pub struct Cli {
+    #[clap(short = 'c', long, action)]
+    /// Prints the current configurations to the terminal with the applied overrides
+    pub show_configs: bool,
+}
+
+impl Cli {
+    /// Resolves the command line arguments and applies the necessary changes to the terminal and app
+    ///
+    /// Some arguments may finish the program early (returning `ControlFlow::Break`)
+    pub fn resolve<B: Backend>(
+        &self,
+        terminal: Terminal<B>,
+        app: &mut App,
+    ) -> ControlFlow<color_eyre::Result<()>, Terminal<B>> {
+        if self.show_configs {
+            Logger::info("Printing current configurations");
+            drop(terminal);
+            if let Err(err) = utils::restore() {
+                return ControlFlow::Break(Err(eyre!(err)));
+            }
+            match serde_json::to_string_pretty(&app.config) {
+                Err(err) => return ControlFlow::Break(Err(eyre!(err))),
+                Ok(config) => println!("patch-hub configurations:\n{}", config),
+            }
+
+            return ControlFlow::Break(Ok(()));
+        }
+
+        ControlFlow::Continue(terminal)
+    }
+}
