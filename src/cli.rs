844408f3ccbe7ac1ff4f307d657b1d8580a46b23
OJarrisonn <j.h.m.t.v.10@gmail.com>
2024-10-16 17:06:10 -0300

    feat: add `--show-configs` option
    This introduces a new `--show-configs` option to patch-hub.
    
    This flag will simply print the config options patch-hub would use to
    the terminal as a json string and exit the application.
    
    Signed-off-by: OJarrisonn <j.h.m.t.v.10@gmail.com>

--- src/cli.rs
+++ src/cli.rs
@@ -2,4 +2,8 @@ use clap::Parser;
 
 #[derive(Debug, Parser)]
 #[command(version, about)]
-pub struct Cli;
+pub struct Cli {
+    #[clap(long, action)]
+    /// Prints the current configurations to the terminal with the applied overrides
+    pub show_configs: bool,
+}
