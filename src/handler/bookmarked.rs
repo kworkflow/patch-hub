34107c3736fc9d91ad098f01308cec3fa8de0457
OJarrisonn <j.h.m.t.v.10@gmail.com>
2024-11-13 22:14:52 -0300

    feat: implement `bookmarked` help popup
    Signed-off-by: OJarrisonn <j.h.m.t.v.10@gmail.com>

--- src/handler/bookmarked.rs
+++ src/handler/bookmarked.rs
@@ -3,6 +3,7 @@ use std::ops::ControlFlow;
 use crate::{
     app::{screens::CurrentScreen, App},
     loading_screen,
+    ui::popup::{help::HelpPopUpBuilder, PopUp},
 };
 use ratatui::{
     crossterm::event::{KeyCode, KeyEvent},
@@ -19,6 +20,10 @@ where
     B: Backend + Send + 'static,
 {
     match key.code {
+        KeyCode::Char('?') => {
+            let popup = generate_help_popup();
+            app.popup = Some(popup);
+        }
         KeyCode::Esc => {
             app.bookmarked_patchsets_state.patchset_index = 0;
             app.set_current_screen(CurrentScreen::MailingListSelection);
@@ -41,4 +46,18 @@ where
         _ => {}
     }
     Ok(ControlFlow::Continue(terminal))
+}
+
+pub fn generate_help_popup() -> Box<dyn PopUp> {
+    let popup = HelpPopUpBuilder::new()
+        .title("Bookmarked Patchsets")
+        .description("This screen shows all the patchsets you have bookmarked.\nThis is quite useful to keep track of patchsets you are interested in take a look later.")
+        .keybind("ESC", "Exit")
+        .keybind("ENTER", "See details of the selected patchset")
+        .keybind("?", "Show this help screen")
+        .keybind("j/🡇", "Down")
+        .keybind("k/🡅", "Up")
+        .build();
+
+    Box::new(popup)
 }
