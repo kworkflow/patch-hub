246965d20f46c1ac623b15f721e1fc82b83ac323
Lorenzo Bertin Salvador <lorenzobs@usp.br>
2025-04-13 18:31:59 -0300

    feat: use config log path and deal with logs before its initialization
    This commit changes the new logging logic so logs are stored in the path
    defined at patch-hub's config. Since logging should encompass the whole
    application lifecycle, we have to correctly deal with logs produced
    before/during Config initialization.
    To solve this firstly we save the logs to a temporary path, and then,
    after Config intitialization we move the existing logs to the path defined
    at the Config and reload the logging layer so new logs are stored in
    the updated path.
    
    Signed-off-by: Lorenzo Bertin Salvador <lorenzobs@usp.br>

--- src/infrastructure/monitoring/logging.rs
+++ src/infrastructure/monitoring/logging.rs
@@ -1,104 +1,79 @@
 use std::{
-    fs::File,
-    io::Write,
-    path::Path,
+    collections::HashMap,
     sync::{Arc, Mutex},
 };
 
+use multi_log_file_writer::{create_non_blocking_writer, get_fmt_layer, MultiLogFileWriter};
 use tracing_appender::non_blocking::WorkerGuard;
-use tracing_subscriber::{Layer, Registry};
+use tracing_subscriber::{reload::Handle, Layer, Registry};
 
-const PATH_HUB_TARGET: &str = "patch_hub";
-const LATEST_LOG_FILENAME: &str = "latest.log";
+pub mod multi_log_file_writer;
+
+const LATEST_LOG_FILENAME: &str = "latest.tracing.log";
 const TEMP_LOG_DIR: &str = "/tmp/temporary-patch-hub-logs";
 
 pub struct InitLoggingLayerProduct {
     pub logging_layer: Box<dyn Layer<Registry> + Send + Sync>,
-    pub file_writer_guards: Vec<WorkerGuard>,
+    pub multi_log_file_writer: MultiLogFileWriter,
+    pub guards_by_file_name: HashMap<String, WorkerGuard>,
+    pub reload_handle: Handle<Box<dyn Layer<Registry> + Send + Sync>, Registry>,
 }
 struct InitLoggingFileWritersProduct {
-    multi_file_writer: MultiFileWriter,
-    file_writer_guards: Vec<WorkerGuard>,
-}
-
-#[derive(Clone)]
-pub struct MultiFileWriter {
-    writers: Vec<Arc<Mutex<dyn Write + Send>>>,
+    multi_log_file_writer: MultiLogFileWriter,
+    guards_by_file_name: HashMap<String, WorkerGuard>,
 }
 
 pub fn init_logging_layer() -> InitLoggingLayerProduct {
     let InitLoggingFileWritersProduct {
-        multi_file_writer,
-        file_writer_guards,
+        multi_log_file_writer,
+        guards_by_file_name,
     } = init_logging_file_writers();
 
-    let filter_patch_hub_target =
-        tracing_subscriber::filter::filter_fn(|metadata| metadata.target() == PATH_HUB_TARGET);
+    let fmt_layer = get_fmt_layer(multi_log_file_writer.clone());
 
-    let logging_layer = Box::new(
-        tracing_subscriber::fmt::layer()
-            .with_writer(move || multi_file_writer.clone())
-            .with_file(true)
-            .with_line_number(true)
-            .with_timer(tracing_subscriber::fmt::time::SystemTime)
-            .json()
-            .with_filter(filter_patch_hub_target),
-    );
+    let (reload_layer, reload_handle) = tracing_subscriber::reload::Layer::new(fmt_layer);
 
     InitLoggingLayerProduct {
-        logging_layer,
-        file_writer_guards,
+        logging_layer: Box::new(reload_layer),
+        multi_log_file_writer,
+        guards_by_file_name,
+        reload_handle,
     }
 }
 
 fn init_logging_file_writers() -> InitLoggingFileWritersProduct {
     let timestamp_log_file_name = format!(
-        "patch-hub_{}.log",
+        "patch-hub_{}.tracing.log",
         chrono::Local::now().format("%Y%m%d-%H%M%S")
     );
 
-    // we have to truncate the file so if the file already exists we overwrite its content
-    let latest_log_path = Path::new(TEMP_LOG_DIR).join(LATEST_LOG_FILENAME);
-    let _ = File::create(&latest_log_path);
-
-    let timestamp_log_file_appender =
-        tracing_appender::rolling::never(TEMP_LOG_DIR, timestamp_log_file_name);
-    let latest_log_file_appender =
-        tracing_appender::rolling::never(TEMP_LOG_DIR, LATEST_LOG_FILENAME);
-
     // logging thread should be non-blocking so it does not interfere with the rest of the application
     let (timestamp_log_writer, timestamp_log_writer_guard) =
-        tracing_appender::non_blocking(timestamp_log_file_appender);
+        create_non_blocking_writer(TEMP_LOG_DIR, timestamp_log_file_name.as_str());
     let (latest_log_writer, latest_log_writer_guard) =
-        tracing_appender::non_blocking(latest_log_file_appender);
+        create_non_blocking_writer(TEMP_LOG_DIR, LATEST_LOG_FILENAME);
+
+    let writer_by_file_name = HashMap::from([
+        (
+            timestamp_log_file_name.clone(),
+            Arc::new(Mutex::new(timestamp_log_writer)),
+        ),
+        (
+            LATEST_LOG_FILENAME.to_string(),
+            Arc::new(Mutex::new(latest_log_writer)),
+        ),
+    ]);
+
+    let guards_by_file_name = HashMap::from([
+        (timestamp_log_file_name, timestamp_log_writer_guard),
+        (LATEST_LOG_FILENAME.to_string(), latest_log_writer_guard),
+    ]);
 
     InitLoggingFileWritersProduct {
-        multi_file_writer: MultiFileWriter::new(vec![
-            Arc::new(Mutex::new(timestamp_log_writer)),
-            Arc::new(Mutex::new(latest_log_writer)),
-        ]),
-        file_writer_guards: vec![timestamp_log_writer_guard, latest_log_writer_guard],
-    }
-}
-
-impl MultiFileWriter {
-    pub fn new(writers: Vec<Arc<Mutex<dyn Write + Send>>>) -> Self {
-        Self { writers }
-    }
-}
-
-impl Write for MultiFileWriter {
-    fn write(&mut self, buf: &[u8]) -> std::io::Result<usize> {
-        for writer in &self.writers {
-            let _ = writer.lock().expect("to get lock").write_all(buf);
-        }
-        Ok(buf.len())
-    }
-
-    fn flush(&mut self) -> std::io::Result<()> {
-        for writer in &self.writers {
-            writer.lock().expect("to get lock").flush()?;
-        }
-        Ok(())
+        multi_log_file_writer: MultiLogFileWriter::new(
+            TEMP_LOG_DIR.to_string(),
+            writer_by_file_name,
+        ),
+        guards_by_file_name,
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -22,7 +22,10 @@ use tracing::{event, Level};
 fn main() -> color_eyre::Result<()> {
     // file writer guards should be propagated to main() so the logging thread lives enough
     let InitMonitoringProduct {
-        file_writer_guards: _file_writer_guards,
+        logging_guards_by_file_name,
+        mut multi_log_file_writer,
+        logging_reload_handle,
+        ..
     } = init_monitoring();
 
     let args = Cli::parse();
@@ -32,6 +35,13 @@ fn main() -> color_eyre::Result<()> {
 
     let config = Config::build();
     config.create_dirs();
+
+    // with the config we can update log directory
+    let _guards = multi_log_file_writer.update_log_writer_with_config(
+        &config,
+        logging_guards_by_file_name,
+        logging_reload_handle,
+    );
 
     match args.resolve(terminal, &config) {
         ControlFlow::Break(b) => return b,
